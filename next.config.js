f5c901170d08cbbb598718148915bc0c4ea72af6
Muthu Palaniyappan OL <muthu892542@gmail.com>
2023-03-28 23:35:00 +0530

    esw

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'EnantraWebsite'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
